3a60fdb9674a09303c05741abb51290e2fc445ac
Prince-samuel-133 <130437757+Prince-samuel-133@users.noreply.github.com>
2024-01-20 15:52:01 +0530

    Update urls.py

--- djangochat/urls.py
+++ djangochat/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns # new
+
 
 urlpatterns = [
     path('', include('core.urls')),
     path('rooms/', include('room.urls')),
     path('admin/', admin.site.urls),
+    urlpatterns += staticfiles_urlpatterns() # new
 ]
